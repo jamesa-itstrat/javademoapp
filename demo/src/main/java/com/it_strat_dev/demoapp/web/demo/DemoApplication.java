4c46246b4a5184b7ab4a686bb089cc1830062043
James Alston <jamesalston@it-strat.com>
2024-06-06 16:43:50 -0400

    Project Files

---
+++ demo/src/main/java/com/it_strat_dev/demoapp/web/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.it_strat_dev.demoapp.web.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
